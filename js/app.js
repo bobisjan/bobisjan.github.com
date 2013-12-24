f3646e2959ff3c033c674099e2711469127d6aba
Jan Bobisud <me@bobisjan.com>
2013-12-24 12:02:24 +0100

    Add template helper for current year

--- js/app.js
+++ js/app.js
@@ -38,6 +38,11 @@ Ember.Handlebars.helper('markdown', function(input) {
   return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
+Ember.Handlebars.helper('year', function() {
+  var now = new Date();
+  return now.getFullYear();
+});
+
 App.Router.map(function() {
 
 });
