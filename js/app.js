9d25bfb091c1b457e109bba03985461352856bdd
Jan Bobisud <me@bobisjan.com>
2013-12-24 11:31:09 +0100

    Use simple hash for link

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 
 App = Ember.Application.create();
 
+var links = [{
+  title: "GitHub",
+  url: "https://github.com/bobisjan/"
+}, {
+  title: "LinkedIn",
+  url: "http://cz.linkedin.com/pub/jan-bobisud/76/802/273/"
+}];
+
 var profiles = [{
   title: "Master's Student",
   email: "bobisjan@fel.cvut.cz",
@@ -26,25 +34,12 @@ Ember.Handlebars.helper('markdown', function(input) {
   return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
-App.Link = Em.Object.extend({
-  title: null,
-  url: null
-});
-
 App.Router.map(function() {
 
 });
 
 App.ApplicationRoute = Em.Route.extend({
   setupController: function(controller, model) {
-    var links = [App.Link.create({
-      title: "GitHub",
-      url: "https://github.com/bobisjan/"
-    }), App.Link.create({
-      title: "LinkedIn",
-      url: "http://cz.linkedin.com/pub/jan-bobisud/76/802/273/"
-    })];
-    
     controller.set('links', links);
   }
 });
