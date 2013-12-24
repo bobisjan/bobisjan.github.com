821302649be39eff132b5e275b83767db75d62f8
Jan Bobisud <me@bobisjan.com>
2013-12-24 10:27:23 +0100

    Make links dynamic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
 
 App = Ember.Application.create();
 
+App.Link = Em.Object.extend({
+  title: null,
+  url: null
+});
+
 App.Router.map(function() {
 
+});
+
+App.ApplicationRoute = Em.Route.extend({
+  setupController: function(controller, model) {
+    var links = [App.Link.create({
+      title: "GitHub",
+      url: "https://github.com/bobisjan/"
+    }), App.Link.create({
+      title: "LinkedIn",
+      url: "http://cz.linkedin.com/pub/jan-bobisud/76/802/273/"
+    })];
+    
+    controller.set('links', links);
+  }
+});
+
+App.ApplicationController = Em.Controller.extend({
+  links: []
 });
